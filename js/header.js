4086210c62b56922eca66e12f1cf4c65e39510f9
furqan <amrahabib@gmail.com>
2022-09-05 17:31:00 +0500

    first commit

---
+++ js/header.js
@@ -0,0 +1,51 @@
+document.getElementById(header).innerHTML=`
+<nav>
+    <div class="logo">
+        <img src="img/logo.png" alt=""height="150px" width="150px">
+    </div>
+    <div class="menu">
+        <a href="#Home">Home</a>
+        <a href="#About">About</a>
+        <a href="#7 WONDER">7 WONDER</a>
+        <a href="Gallery.html">Gallery</a>
+        <a href="#Contact">Contact</a>
+         </div>
+  </nav>
+  <section class="Home" id="home">
+  <div class="h-text">
+    <h2>WONDER OF THE WORLD</h2>
+    <p>2000 YEARS AFTER THE ANCIENT 7 WONDERS HAVE BEEN DEFINED,<br>
+       WE CREATE GLOBAL MEMORY AGAIN WITH THE NEW 7 WONDERS OF THE WORLD..
+
+    </p>
+
+  </div>
+  </section>
+  
+  
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="img/christ the redeemer.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/great wall of chaina.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/pyramid of egypt.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/taj mehal.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/the colosseum.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/the eiffel tower.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="img/the eiffel tower.jpg" />
+      </div>
+    </div>
+   </div>
+</header>`
